049ba9589710428ea557fef0e253dc91cdd3eb70
Abhishek9690 <abhishek.gupta_cs18@gla.ac.in>
2019-09-04 15:30:01 -0400

    Initial commit

---
+++ src/Lab04/Main.java
@@ -0,0 +1,11 @@
+package Lab04;
+
+import Lab04.Person;
+
+public class Main {
+    public static void main(String[] args) {
+        Person p2 = new Person("abhishek", 18, 'M');
+        System.out.println(p2.toString());
+    }
+}
+
